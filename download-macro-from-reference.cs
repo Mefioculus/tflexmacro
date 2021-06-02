fb97f390312b0f957788866ba894ce44ee791a82
Mefioculus <lin0ge@yandex.ru>
2021-06-02 09:37:35 +0300

    implement method and service class for reading data.

--- download-macro-from-reference.cs
+++ download-macro-from-reference.cs
@@ -18,6 +18,20 @@ public class Macro : MacroProvider {
 
     // Данный макрос предназначен для выгрузки всех макросов в определенную директорию
 
+    #region Guids
+
+    private static class Guids {
+        private static class References {
+            private static Guid MacroReference = new Guid("3e6df4d0-b1d8-4375-978c-4da676604cca");
+        }
+
+        private static class Properties {
+            private static Guid Name = new Guid("8334853d-8b04-4716-b3e2-19bc0a360384");
+            private static Guid Code = new Guid("3d654359-8567-49b3-8060-516f5f2f2ad2");
+        }
+    }
+
+    #endregion Guids
 
     #region Properties
 
@@ -31,6 +45,9 @@ public class Macro : MacroProvider {
     #region Entry point
 
     public override void Run() {
+        // Получаем список всех макросов, которые есть в справочнике
+        List<MacrosObject> listOfMacros = GetListOfMacro();
+
         GetFolderForExportMacro();
         Check();
         ExportMacro();
@@ -61,12 +78,47 @@ public class Macro : MacroProvider {
 
     }
 
+    private List<MacrosObject> GetListOfMacro() {
+        // TODO Реализовать метод, который получает перечень всех макросов
+        List<MacrosObject> listOfMacros = new List<MacrosObject>();
+        string template = "Количество найденных макросов в справочнике - {0} шт.";
+        string message = string.Empty;
+
+        Reference macroReference = Context.Connection.ReferenceCatalog.Find(Guids.References.MacroReference).CreateReference();
+
+        foreach (ReferenceObject refObj in macroReference.Objects) {
+            MacrosObject macros = new MacrosObject();
+            
+            // Создание объекта, который будет хранить все основные параметры макроса
+            macros.GuidOfMacro = refObj.SystemFields.Guid;
+            macros.Name = refObj[Guids.Properties.Name];
+            macros.Code = refObj[Guids.Properties.Code];
+            macros.DateLastModification = refObj.SystemFields.EditDate;
+
+            listOfMacros.Add(macros);
+            message += string.Format("{0}\n", macros.Name);
+        }
+
+        Message("Информация", string.Format(template, listOfMacros.Count));
+        Message("Список макросов", message);
+
+        return listOfMacros;
+    }
+
     #endregion Service methods
 
     #region Service classes
 
     private class StatusExport {
         // TODO Реализовать класс для хранения данных об экспорте
+    }
+
+    private class MacrosObject {
+        // TODO Реализовать класс, которырый будет хранить в себе данные о макросе
+        public Guid GuidOfMacro { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public DateTime DateLastModification { get; set; }
     }
 
     #endregion Service classes
