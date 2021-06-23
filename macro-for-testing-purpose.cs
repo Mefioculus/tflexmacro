4d3a240710f1c372c9c582c25955a00d19b9c6c1
Mefioculus <lin0ge@yandex.ru>
2021-06-23 15:05:10 +0300

    Add code for search cz by access

--- macro-for-testing-purpose.cs
+++ macro-for-testing-purpose.cs
@@ -35,3 +35,38 @@ private bool СодержитПапку(Объект дсе) {
     return false;
 }
 #endregion Рекурсивное получение списка изделий для случайной ДСЕ
+
+#region Получение списка цехопереходов, которые наследуют свои доступы
+
+
+// Для работы данного кода потребуются так же следующие пространства имен:
+//using TFlex.DOCs.Model.References;
+//using TFlex.DOCs.Model.Access;
+//using TFlex.DOCs.Model.Classes;
+// А так же возможно ссылки на следующие библиотеки
+//Cs.UI.Objects.dll
+//TFlex.DOCs.UI.Common.dll
+//TFlex.DOCs.UI.Types.dll
+//TFlex.DOCs.Common.dll
+
+
+Reference techReference = Context.Connection.ReferenceCatalog.Find(new Guid("353a49ac-569e-477c-8d65-e2c49e25dfeb")).CreateReference();
+
+// Получаем список технологический процессов
+techReference.Objects.Load();
+
+string message = "Список цехопереходов с явными правами:";
+
+foreach (ReferenceObject process in techReference.Objects) {
+    // Получаем список цехопереходов
+    foreach (ReferenceObject perehod in process.Children) {
+        AccessManager accessManager = AccessManager.GetReferenceObjectAccess(perehod);
+        if (!accessManager.IsInherit) {
+            message += string.Format("\nТП (id {0}): {1}; ЦЗ (id {2}): {3}", process.SystemFields.Id.ToString(), process.ToString(), perehod.SystemFields.Id.ToString(), perehod.ToString());
+        }
+    }
+}
+
+Message("Информация", message);
+
+#endregion Получение списка цехопереходов, которые наследуют свои доступы
