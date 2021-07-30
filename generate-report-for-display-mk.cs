c11c120eae1d3207f24f8021ccf0282482f20823
Mefioculus <lin0ge@yandex.ru>
2021-07-30 15:26:17 +0300

    Add fetching data from tflex, add enums in specRow

--- generate-report-for-display-mk.cs
+++ generate-report-for-display-mk.cs
@@ -31,7 +31,7 @@ public class Macro : MacroProvider {
     private string pathToSourceDirectoryFoxProDb = @"\\fs\FoxProDB\COMDB\PROIZV";
     private string pathToTempDirectoryFoxProDb = 
         Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp");
-    private string[] arrayOfDbFiles = new string[] {"spec.dbf", "mars.dbc", "mars.dct"};
+    private string[] arrayOfDbFiles = new string[] {"spec.dbf", "trud.dbf"};
 
     // Поля для хранения классов и методов, необходимых для использования библиотеки
     // через Reflection
@@ -39,6 +39,24 @@ public class Macro : MacroProvider {
     private MethodInfo readMethod;
     private MethodInfo getStringMethod;
     private MethodInfo closeMethod;
+    
+    // Дебаг
+    private bool debug = true;
+
+    // Класс с уникальными идентификаторами, которые используются в данном макросе
+    private static class Guids {
+        public static class References {
+            public static Guid АрхивОГТ = new Guid("500d4bcf-e02c-4b2e-8f09-29b64d4e7513");
+            
+        }
+        public static class Files {
+            public static Guid dbfDataReaderLibFile = new Guid("e9f3174e-43d0-46c9-9b57-35b5733a5131");
+        }
+        public static class Parameters {
+            public static Guid АрхивОГТ_обозначениеДеталиУзла = new Guid("c11b5a98-c22c-42bc-8375-be30052ffba2");
+            public static Guid АрхивОГТ_сканДокумента = new Guid("5947d0ce-b096-4791-96a4-e3ac03f9c49c");
+        }
+    }
 
     // Опции для проведения чтения dbf таблиц
     DbfDataReader.DbfDataReaderOptions dataReaderOptions = new DbfDataReader.DbfDataReaderOptions() {
@@ -74,7 +92,6 @@ public class Macro : MacroProvider {
 
         // Удаляем все скопированные ранее файлы баз данных данных,
         // так как в дальнейшем они не нужны
-        DeleteTemporaryFiles();
         Message("Информация", "Работа макроса успешно завершена");
     }
     #endregion Entry Points
@@ -87,12 +104,15 @@ public class Macro : MacroProvider {
         string message = string.Empty;
         string headTemplate = "Во время копирования файлов базы данных не были обнаружены следующие файлы:\n";
         string regularTemplate = "- {0};\n";
+
         foreach (string file in arrayOfDbFiles) {
             string source = Path.Combine(pathToSourceDirectoryFoxProDb, file);
             string destination = Path.Combine(pathToTempDirectoryFoxProDb, file);
             
             if (File.Exists(source)) {
-                File.Copy(source, destination, true);
+                // Перед копированием проводим проверку на то, необходимо ли производить копирование
+                if (NeedACopy(source, destination))
+                    File.Copy(source, destination, true);
             }
             else {
                 if (message == string.Empty) {
@@ -101,6 +121,33 @@ public class Macro : MacroProvider {
                 message += string.Format(regularTemplate, file);
             }
         }
+    }
+
+    private bool NeedACopy (string sourceFile, string targetFile) {
+        string fileName = Path.GetFileNameWithoutExtension(sourceFile);
+        string message = string.Empty;
+
+        // Если файла в директории назначения не сущестует, возвращаем истину
+        if (!File.Exists(targetFile)) {
+            message = "В директории с копиями файлов не был обнаружен файл {0} базы данных FoxPro\n";
+            message += "Будет произведено копирование данного файла";
+            if (debug)
+                Message("Информация", string.Format(message,  fileName));
+            return true;
+        }
+
+        // Если файлы различаются, возвращаем истину
+        DateTime sourceFileModification = File.GetLastWriteTime(sourceFile);
+        DateTime targetFileModification = File.GetLastWriteTime(targetFile);
+        if (sourceFileModification != targetFileModification) {
+            message = "У источника и копии файла '{0}' разные даты модификации:\nФайл источник - {1}\nКопия файла - {2}\n";
+            message += "Будет произведено копирование данного файла";
+            if (debug)
+                Message("Информация", string.Format(message, fileName, sourceFileModification, targetFileModification));
+            return true;
+        }
+
+        return false;
     }
 
     private void DeleteTemporaryFiles() {
@@ -141,7 +188,7 @@ public class Macro : MacroProvider {
         FileReference fileReference = new FileReference(Context.Connection);
         
         // Находим файл библиотеки
-        FileObject libFile = fileReference.Find(new Guid("e9f3174e-43d0-46c9-9b57-35b5733a5131")) as FileObject;
+        FileObject libFile = fileReference.Find(Guids.Files.dbfDataReaderLibFile) as FileObject;
 
         if (libFile == null) {
             Message("Ошибка", "В файловом справочнике не была обнаружена библиотека для работы с dbf файлами");
@@ -258,9 +305,49 @@ public class Macro : MacroProvider {
 
     #endregion Methods for getting composition of product
 
+    #region Fetchng data about mk from Tflex and FoxPro
     private void FetchInfoAboutMK(Dictionary<string, List<SpecRow>> data) {
-
+        foreach (KeyValuePair<string, List<SpecRow>> kvp in data) {
+            FetchDataFromTFlex(kvp.Value);
+            FetchDataFromFox(kvp.Value);
+            SetStatus(kvp.Value);
+        }
     }
+
+    // Метод для получения сведений о наличии МК в архиве ОГТ
+    private void FetchDataFromTFlex(List<SpecRow> table) {
+        // Получаем справочник архива ОГТ
+        Reference archiveOgt = Context.Connection.ReferenceCatalog.Find(Guids.References.АрхивОГТ).CreateReference();
+        TFlex.DOCs.Model.Structure.ParameterInfo shifrParam =
+            archiveOgt.ParameterGroup.Parameters.Find(Guids.Parameters.АрхивОГТ_обозначениеДеталиУзла);
+
+        foreach (SpecRow record in table) {
+            // Производим поиск объектов
+            ReferenceObject archiveRecord = archiveOgt.FindOne(shifrParam, record.Shifr);
+
+            // Если получилось найти соотвутствующую запись в архиве, тогда проверяем значение в колонке
+            // "АрхивОГТ_сканДокумента"
+            if (archiveRecord != null) {
+                record.TFlexMK = archiveRecord[Guids.Parameters.АрхивОГТ_сканДокумента].GetString() != string.Empty ? 
+                    StatusMKArchive.Exist : StatusMKArchive.NotExist;
+            }
+            else
+                record.TFlexMK = StatusMKArchive.NotFound;
+        }
+
+        // Получаем все объекты справочника
+    }
+
+    // Метод для получения сведений о том, какие сведения нормы на данную ДСЕ
+    // в FoxPro
+    private void FetchDataFromFox(List<SpecRow> table) {
+        // Получаем таблицу TRUD
+    }
+
+    // Установка статуса текущей ДСЕ
+    private void SetStatus(List<SpecRow> table) {
+    }
+    #endregion Fetchng data about mk from Tflex and FoxPro
 
     #region Methods for generate output report file
 
@@ -282,26 +369,62 @@ public class Macro : MacroProvider {
     #endregion Methods for generate output report file
 
     #region serviceClasses
+
+    #region Enums
+    private enum StatusMKArchive {
+        NotProcessed, // Дефолтный статус
+        Exist, // Маршрутная карта существует в Архиве ОГТ
+        NotExist, // Маршрутная карту не существует а Архиве ОГТ
+        NotFound // Данная ДСЕ не была обнаружена в Архиве ОГТ
+    }
+
+    private enum StatusMKFoxPro {
+        NotProcessed, // Дефолтный статус
+        Locked, // В Fox заблокировано редактирование технологии
+        Unlocked // В Fox разблокировано редактирование технологии
+    }
+
+    private enum StatusOfDSE {
+        NotProcessed, // Дефолтный статус
+        Done, // Технология готова (есть и в Fox и в Архиве)
+        NeedQualification, // Требует внимания, так как результат неожидаемый
+        Reworking, // Технология есть только в Fox
+        Creating // Технологии нет ни в Fox на в Архиве
+    }
+    #endregion Enums
+
     private class SpecRow {
         public string Shifr { get; set; }
         public string Name { get; set; }
         public string Parent { get; set; }
-        public bool FoxProMK { get; set; }
-        public bool TFlexMK { get; set; }
-        public string Status { get; set; }
+        public StatusMKFoxPro FoxProMK { get; set; }
+        public StatusMKArchive TFlexMK { get; set; }
+        public StatusOfDSE Status { get; set; }
 
         public SpecRow (string shifr, string name, string parent) {
             this.Shifr = shifr;
             this.Name = name;
             this.Parent = parent;
+
+            this.TFlexMK = StatusMKArchive.NotProcessed;
+            this.FoxProMK = StatusMKFoxPro.NotProcessed;
+            this.Status = StatusOfDSE.NotProcessed;
         }
 
         public override string ToString() {
-            return string.Format("{0};{1};{2}", Shifr, Name, Parent);
+            return string.Format(
+                    "{0};{1};{2};{3};{4};{5}",
+                    Shifr,
+                    Name,
+                    Parent,
+                    FoxProMK.ToString(),
+                    TFlexMK.ToString(),
+                    Status.ToString()
+                    );
         }
 
         public static string GetHeader() {
-            return "Шифр;Наименование;Родитель\n";
+            return "Шифр;Наименование;Родитель;Наличие в FoxPro;Наличие в Архиве ОГТ;Статус\n";
         }
     }
     #endregion serviceClasses
