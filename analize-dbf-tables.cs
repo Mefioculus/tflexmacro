fbd9fd00228de4ab1c139c1af460ca147a071159
Mefioculus <lin0ge@yandex.ru>
2022-01-20 17:20:05 +0300

    Добавлен метод для сравнения двух репозиториев

--- analize-dbf-tables.cs
+++ analize-dbf-tables.cs
@@ -37,47 +37,57 @@ public class Macro : MacroProvider {
     };
 
     public override void Run() {
+        Message("Информация", Path.GetPathRoot(sourcePath));
+        Message("Информация", Path.GetPathRoot(backupPath));
+
+
         DbRepository sourceRepository = new DbRepository(TypeRepository.Source, sourcePath);
         Message("Информация", sourceRepository.ToString());
+
+        DbRepository backupRepository = new DbRepository(TypeRepository.Backup, backupPath);
+        Message("Информация", backupRepository.ToString());
     }
 
     private class DbRepository {
         public TypeRepository Type { get; private set; } = TypeRepository.None;
         public StatusRepository Status { get; private set; } = StatusRepository.Empty;
         public string Dir { get; private set; }
-        public List<FileInfo> DbfFiles { get; private set; }
-        public List<FileInfo> MetaFiles { get; private set; }
-        public List<FileInfo> OtherFiles { get; private set; }
+        public Dictionary<string, FileInfo> DbfFiles { get; private set; }
+        public Dictionary<string, FileInfo> MetaFiles { get; private set; }
+        public Dictionary<string, FileInfo> OtherFiles { get; private set; }
         public int Count { get; private set; }
         public int CountDbf => this.DbfFiles.Count;
         public int CountMeta => this.MetaFiles.Count;
         public int CountOther => this.OtherFiles.Count;
+        public bool CanWrite { get; private set; } = false;
 
         public DbRepository(TypeRepository type, string pathToDir) {
             if (Directory.Exists(pathToDir)) {
+                // Присваиваем исходные параметры
                 this.Type = type;
                 this.Dir = pathToDir;
+
+                // Начинаем обработку файлов
                 string[] files = Directory.GetFiles(this.Dir);
                 this.Count = files.Length;
 
-                this.DbfFiles = new List<FileInfo>(this.Count);
-                this.MetaFiles = new List<FileInfo>(this.Count);
-                this.OtherFiles = new List<FileInfo>(this.Count);
+                this.DbfFiles = new Dictionary<string, FileInfo>(this.Count);
+                this.MetaFiles = new Dictionary<string, FileInfo>(this.Count);
+                this.OtherFiles = new Dictionary<string, FileInfo>(this.Count);
 
                 // Производим добавление информации о файлах
                 foreach (string file in files) {
                     FileInfo fileInfo = new FileInfo(file.ToLower());
                     if (fileInfo.Extension == ".dbf") {
-                        this.DbfFiles.Add(fileInfo);
+                        this.DbfFiles.Add(fileInfo.Name, fileInfo);
                         continue;
                     }
                     if (metaFilesExtensions.Contains(fileInfo.Extension)) {
-                        this.MetaFiles.Add(fileInfo);
+                        this.MetaFiles.Add(fileInfo.Name, fileInfo);
                         continue;
                     }
-                    this.OtherFiles.Add(fileInfo);
+                    this.OtherFiles.Add(fileInfo.Name, fileInfo);
                 }
-
             }
 
         }
@@ -97,6 +107,23 @@ public class Macro : MacroProvider {
                     this.CountOther.ToString(),
                     this.Count.ToString()
                     );
+        }
+
+        public string Compare(DbRepository otherRepository) {
+            DbRepository source;
+            DbRepository backup;
+
+            // Определяем, какой репозиторий с каким будет сравниваться
+            if (otherRepository.Type == TypeRepository.Source) {
+                source = otherRepository;
+                backup = this;
+            }
+            else {
+                source = this;
+                backup = otherRepository;
+            }
+
+            return string.Empty;
         }
 
     }
