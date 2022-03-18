6104309b99d1acabbbd49fd5057ffb4557ec4569
Mefioculus <lin0ge@yandex.ru>
2022-03-18 16:06:36 +0300

    Добавлена обработка параметров, добавлен общий интерфейс для нод, а так же добавлены параметры, который пригодятся для проведения сравнения объектов

--- compare-structure-tflex-bases.cs
+++ compare-structure-tflex-bases.cs
@@ -49,6 +49,18 @@ public class Macro : MacroProvider {
     }
 
 
+    public interface IStructureNode {
+        public Guid Guid { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
+
+        public Dictionary<Guid, IStructureNode> GetNodes();
+        public string ToString(int currPadding, int deltaPadding);
+    
+    }
 
     public class StructureDataBase {
         public string Name { get; set; }
@@ -59,7 +71,7 @@ public class Macro : MacroProvider {
 
             this.References = new Dictionary<Guid, StructureReference>();
             foreach (ReferenceInfo refInfo in connection.ReferenceCatalog.GetReferences()) {
-                this.References.Add(refInfo.Guid, new StructureReference(refInfo));
+                this.References.Add(refInfo.Guid, new StructureReference(refInfo, this));
             }
         }
 
@@ -69,22 +81,33 @@ public class Macro : MacroProvider {
         }
     }
 
-    public class StructureReference {
+    public class StructureReference : IStructureNode {
         public Guid Guid { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
         
         public Dictionary<Guid, StructureClass> Classes { get; set; }
 
-        public StructureReference(ReferenceInfo referenceInfo) {
+        public StructureReference(ReferenceInfo referenceInfo, StructureDataBase root, IStructureNode parent = null) {
             this.Guid = referenceInfo.Guid;
             this.ID = referenceInfo.Id;
             this.Name = referenceInfo.Name;
+            this.Parent = parent;
+            this.Root = root;
+            this.Status = CompareResult.НеОбработано;
 
             this.Classes = new Dictionary<Guid, StructureClass>();
             foreach (ClassObject classObject in referenceInfo.Classes.AllClasses.Where(cl => cl is ClassObject)) {
-                this.Classes.Add(classObject.Guid, new StructureClass(classObject));
+                this.Classes.Add(classObject.Guid, new StructureClass(classObject, this.Root, this));
             }
+        }
+
+        public Dictionary<Guid, IStructureNode> GetNodes() {
+            // TODO: Реализовать метод получения всех дочерних нод
+            return null;
         }
 
         public string ToString(int currPadding, int deltaPadding) {
@@ -93,26 +116,37 @@ public class Macro : MacroProvider {
         }
     }
 
-    public class StructureClass {
+    public class StructureClass : IStructureNode {
         public Guid Guid { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
         
         public Dictionary<Guid, StructureParameterGroups> ParameterGroups { get; set; }
         public Dictionary<Guid, StructureLink> Links { get; set; }
 
-        public StructureClass(ClassObject classObject) {
+        public StructureClass(ClassObject classObject, StructureDataBase root, IStructureNode parent = null) {
             this.Guid = classObject.Guid;
             this.ID = classObject.Id;
             this.Name = classObject.Name;
+            this.Parent = parent;
+            this.Root = root;
+            this.Status = CompareResult.НеОбработано;
 
             this.ParameterGroups = new Dictionary<Guid, StructureParameterGroups>();
             this.Links = new Dictionary<Guid, StructureLink>();
 
             // Производим поиск групп параметров справочника
             foreach (ParameterGroup group in classObject.GetAllGroups()) {
-                this.ParameterGroups.Add(group.Guid, new StructureParameterGroups(group));
+                this.ParameterGroups.Add(group.Guid, new StructureParameterGroups(group, this.Root, this));
             }
+        }
+
+        public Dictionary<Guid, IStructureNode> GetNodes() {
+            // TODO: Реализовать метод получения всех дочерних нод
+            return null;
         }
 
         public string ToString(int currPadding, int deltaPadding) {
@@ -122,46 +156,103 @@ public class Macro : MacroProvider {
         
     }
 
-    public class StructureParameterGroups {
+    public class StructureParameterGroups : IStructureNode {
         public Guid Guid { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
 
         public Dictionary<Guid, StructureParameter> Parameters { get; set; }
 
-        public StructureParameterGroups(ParameterGroup group) {
+        public StructureParameterGroups(ParameterGroup group, StructureDataBase root, IStructureNode parent = null) {
             this.Guid = group.Guid;
             this.ID = group.Id;
             this.Name = group.Name;
+            this.Parent = parent;
+            this.Root = root;
+            this.Status = CompareResult.НеОбработано;
+
+            this.Parameters = new Dictionary<Guid, StructureParameter>();
+
+            // Получаем параметры из группы параметров
+            foreach (ParameterInfo parameterInfo in group.Parameters) {
+                this.Parameters.Add(parameterInfo.Guid, new StructureParameter(parameterInfo, this.Root, this));
+            }
+        }
+
+        public Dictionary<Guid, IStructureNode> GetNodes() {
+            // TODO: Реализовать метод получения всех дочерних нод
+            return null;
         }
 
         public string ToString(int currPadding, int deltaPadding) {
             string stringPadding = currPadding == 0 ? string.Empty : new string(' ', currPadding);
-            return $"{stringPadding}(группа) {this.Name}";
+            return $"{stringPadding}(группа) {this.Name}\n{string.Join("\n", this.Parameters.Select(kvp => kvp.Value.ToString(currPadding + deltaPadding, deltaPadding)))}";
         }
     }
 
-    public class StructureParameter {
+    public class StructureParameter : IStructureNode {
         public Guid Guid { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
 
-        public StructureParameter(int id, Guid guid, string name) {
-            this.Guid = guid;
-            this.ID = id;
-            this.Name = name;
+        public StructureParameter(ParameterInfo parameterInfo, StructureDataBase root, IStructureNode parent = null) {
+            this.Guid = parameterInfo.Guid;
+            this.ID = parameterInfo.Id;
+            this.Name = parameterInfo.Name;
+            this.Root = root;
+            this.Parent = parent;
+            this.Status = CompareResult.НеОбработано;
+        }
+
+        public Dictionary<Guid, IStructureNode> GetNodes() {
+            // TODO: Реализовать метод получения всех дочерних нод
+            return null;
+        }
+
+        public string ToString(int currPadding, int deltaPadding) {
+            string stringPadding = currPadding == 0 ? string.Empty : new string(' ', currPadding);
+            return $"{stringPadding}(Параметр) {this.Name}";
         }
     }
 
-    public class StructureLink {
+    public class StructureLink : IStructureNode {
         public Guid Guid { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
+        public IStructureNode Parent { get; set; }
+        public StructureDataBase Root { get; set; }
+        public CompareResult Status { get; set; }
 
         public StructureLink(int id, Guid guid, string name) {
             this.Guid = guid;
             this.ID = id;
             this.Name = name;
         }
+
+        public Dictionary<Guid, IStructureNode> GetNodes() {
+            // TODO: Реализовать метод получения всех дочерних нод
+            return null;
+        }
+
+        public string ToString(int currPadding, int deltaPadding) {
+            string stringPadding = currPadding == 0 ? string.Empty : new string(' ', currPadding);
+            return string.Empty;
+        }
+    }
+
+    public enum CompareResult {
+        НеОбработано,
+        ПолноеСоответствие,
+        Отсутствует,
+        Новый,
+        ОтличаетсяНаименование,
+        ОтличаетсяID,
+        ОтличаетсяНаименованиеИID
     }
 }
