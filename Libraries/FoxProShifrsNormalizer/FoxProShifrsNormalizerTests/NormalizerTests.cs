375b23b435af098162f92df7782d56387fd5878f
Mefioculus <lin0ge@yandex.ru>
2022-05-23 12:46:47 +0300

    Добавление тестов

--- Libraries/FoxProShifrsNormalizer/FoxProShifrsNormalizerTests/NormalizerTests.cs
+++ Libraries/FoxProShifrsNormalizer/FoxProShifrsNormalizerTests/NormalizerTests.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.IO;
 using Xunit;
+using FoxProShifrsNormalizer;
 
-namespace FoxProShifrsNormalizerTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
 
+namespace FoxProShifrsNormalizerTests {
+    public class FirstTest {
+
+        public static List<object[]> Data() {
+            Normalizer normalizer = new Normalizer();
+            List<object[]> result = new List<object[]>();
+            foreach (string shifr in File.ReadAllLines("shifrs.txt")) {
+                result.Add(
+                        new object[] { shifr,  normalizer.NormalizeShifrsFromFox(normalizer.NormalizeShifrsToFox(shifr))}
+                        );
+            }
+            return result;
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestNormalize(string expected, string processed) {
+            Assert.Equal(expected, processed);
         }
     }
 }
