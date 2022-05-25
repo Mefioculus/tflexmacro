02fc2e27c0ee91ec98f965feaebb8a8978ac5e0d
Mefioculus <lin0ge@yandex.ru>
2022-05-25 17:04:09 +0300

    Корректировка метода формирования предварительного отчета для того, чтобы его можно было вызывать из БП для формирования предварительного в начале процесса и после стадий корректировки

--- T-Flex_DOCs/Макросы_для_ЦЗЛ.cs
+++ T-Flex_DOCs/Макросы_для_ЦЗЛ.cs
@@ -372,17 +372,16 @@ public class Macro : MacroProvider
     }
 
     //Формирование отчета для предварительного просмотра
-    public void СформироватьОтчетДляПредварительногоПросмотра() {
+    public void СформироватьОтчетДляПредварительногоПросмотра(ReferenceObject protocol) {
         // Получаем текущий объект
-        UpdateEquipment(); // Вызов метода для того, чтобы в отчет попало актуальное состояние добавленного оборудования
-        ReferenceObject currentObject = Context.ReferenceObject;
-        if (currentObject == null) {
+        UpdateEquipment(protocol); // Вызов метода для того, чтобы в отчет попало актуальное состояние добавленного оборудования
+        if (protocol == null) {
             Message("Ошибка", "Не получилось обратиться к текущему объекту для формирования отчета");
             return;
         }
 
         // Получаем контекст формирования отчета
-        ReportGenerationContext reportContext = new ReportGenerationContext(currentObject, null);
+        ReportGenerationContext reportContext = new ReportGenerationContext(protocol, null);
         reportContext.OpenFile = true;
 
         // Получаем объект отчета
@@ -392,6 +391,15 @@ public class Macro : MacroProvider
         report.Generate(reportContext);
 
         Desktop.CheckIn(reportContext.ReportFileObject, "Предварительный просмотр", false);
+    }
+
+    public void СформироватьОтчетДляПредварительногоПросмотра() {
+        СформироватьОтчетДляПредварительногоПросмотра(Context.ReferenceObject);
+    }
+
+    public void СформироватьОтчетДляПредварительногоПросмотра(Объекты объекты) {
+        foreach (Объект вложение in объекты)
+            СформироватьОтчетДляПредварительногоПросмотра((ReferenceObject)вложение);
     }
 
     #endregion Методы для запуска генерации отчета
@@ -424,9 +432,8 @@ public class Macro : MacroProvider
         
     }
 
-    public void UpdateEquipment() {
-        //ReferenceObject currentProtocol = currentEquip.MasterObject; // Получаем протокол
-        ReferenceObject currentProtocol = Context.ReferenceObject;
+    public void UpdateEquipment(ReferenceObject currentProtocol) {
+        //ReferenceObject currentProtocol = Context.ReferenceObject;
 
         string result = string.Join(
                 ":\n",
